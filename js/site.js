e4f20fac3f29a66064eb530253142faf45c8410b
lolbyaka <crytekunitytek@gmail.com>
2016-06-18 17:25:41 +0300

    че.

--- js/site.js
+++ js/site.js
@@ -1,14 +1,17 @@
-var data_to = new Date(2016, 12, 1), //Дата в формате год, месяц(Отсчет начинается с нуля) и день.
-    time_left = Math.floor((data_to - (new Date())) / 1000); //Высчитываем оставшееся время в мс.
+var data = new Date(),
+    targetTime = new Date();
+    targetTime.setMinutes(targetTime.getMinutes()+10);
+    targetTime.setSeconds(targetTime.getSeconds()+00);
+var time_left = Math.floor((targetTime.valueOf()-data.valueOf())/1000),
     cimg=1;
+
 function parseTime(time_count){
-    //Переменные с количеством дней, часов, минут и секунд.
+
     var day       = Math.floor((time_count/60/60) / 24);
         left_hour = Math.floor(time_count/60/60);
         mins      = Math.floor((time_count - left_hour*60*60)/60);
         secs      = Math.floor(time_count - left_hour *60*60 - mins*60);
         hour      = Math.floor( (time_count - day*24*60*60) / 60 / 60 );
-    //Парсим значения и разно
 
     if(String(hour).length > 1){
         $('div.hours').text(""+(Math.floor(hour / 10))+""+(hour%10));
@@ -35,11 +38,12 @@ function parseTime(time_count){
       }
   }
       $(document).ready(function(){
+
           setInterval(function(){
               time_left--;
               parseTime(time_left);
               if(time_left <= 0){
-                  alert('Время истекло.');
+                  time_left = 0;
               }
           }, 1000);
 
