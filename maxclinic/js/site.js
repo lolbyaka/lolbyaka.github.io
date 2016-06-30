98fd446309a0675d3a0c06c122b73d7d99bc5b36
lolbyaka <crytekunitytek@gmail.com>
2016-07-01 00:24:26 +0300

    222

---
+++ maxclinic/js/site.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$(go).click( function(event){
+		$('#overlay').fadeIn(400,
+		 	function(){
+				$('#modal_form')
+					.css('display', 'block')
+					.animate({opacity: 1, top: '50%'}, 200);
+		});
+	});
+
+	$('#overlay').click( function(){
+		$('#modal_form')
+			.animate({opacity: 0, top: '45%'}, 200,
+				function(){
+					$(this).css('display', 'none');
+					$('#overlay').fadeOut(400);
+				}
+			);
+	});
+});
