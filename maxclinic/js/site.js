d4c8d2868dd0ee678976c6a9d5c89b18b87fa23c
lolbyaka <crytekunitytek@gmail.com>
2016-07-02 13:52:55 +0300

    222

--- maxclinic/js/site.js
+++ maxclinic/js/site.js
@@ -1,6 +1,15 @@
-var count = 0;
-
+var count = 1;
+    now = new Date();
+    day = ""+now.getDate();
+    mounth = ""+(now.getMonth()+1);
 $(document).ready(function() {
+  if(day.length==1){
+    day = "0"+day;
+  }
+  if(mounth.length==1){
+    mounth = "0"+mounth;
+  }
+  $('.promo_add').text("*при заказе до "+day+"/"+mounth)
   $(go).click( function(event){
 		$('#overlay').fadeIn(400,
 		 	function(){
@@ -8,10 +17,19 @@ $(document).ready(function() {
 					.css('display', 'block')
 					.animate({opacity: 1, top: '50%'}, 200);
 		});
-    $(".up").click(function(){
-      alert("The paragraph was clicked.");
-  });
 	});
+
+  $(".up").click(function(){
+    count++;
+    $('.count_number').text(count);
+  });
+  $(".down").click(function(){
+    count--;
+    if(count<=1){
+      count = 1;
+    }
+    $('.count_number').text(count);
+  });
 
 	$('#overlay').click( function(){
 		$('#modal_form')
