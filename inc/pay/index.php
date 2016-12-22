1773b17fbe04fcd07cf08270e9e95ef7e47c5d9a
Алексей <crytekunitytek@gmail.com>
2016-12-22 12:11:54 +0300

    11

---
+++ inc/pay/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Информация об оплате</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+    <link rel="icon" type="image/png" href="favicon.png" />
+    <style>
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: "Roboto";
+      }
+      header {
+        padding: 10px 0;
+        background-color: #9090ae;
+      }
+      .logo {
+        display: inline-block;
+        height: 65px;
+
+      }
+      .logo img {
+        height: 100%;
+      }
+      h1 {
+        display: inline-block;
+        color: #fff;
+        font-weight: 300;
+        text-align: right;
+        right: 0;
+        position: absolute;
+        top: 10%;
+        transform: translateY(-50%);
+      }
+      p {
+        display: inline-block;
+        color: #fff;
+        font-weight: 300;
+        text-align: right;
+        right: 0;
+        position: absolute;
+        top: 60%;
+        transform: translateY(-50%);
+      }
+      .container {
+        width: 980px;
+        margin: 0 auto;
+        position: relative;
+      }
+    </style>
+</head>
+
+<body>
+<header>
+  <div class="container">
+    <a href="http://masaa.ru/"><div class="logo"><img src="http://inc.masaa.ru/logo.png" alt="Masaa"></div></a>
+    <h1>Финансовая информация</h1>
+    <p>Редакция от «1» декабря 2016 г.</p>
+  </div>
+</header>
+<div style="width: 1000px; margin: 0 auto; padding: 50px 0; text-align: justify;">
+<br><br><span style="text-align: center !important; display: block; font-weight: 500;">1 Платежи. Способы оплаты</span>
+<br><br>1.1 Наш сайт подключен к системе приема платежей Единая Касса, и Вы можете оплатить Товар банковской картой Visa или Mastercard, через СберБанк онлайн, яндекс деньгами и многими другими свобобами.
+<br><br><span style="text-align: center !important; display: block; font-weight: 500;">2 Платежная система PayU</span>
+<br><br>2.1 При выборе данной формы оплаты  заказа  на сайте http://masaa.ru/ Вы будете автоматически перенаправлены на платежную форму процессингового центра PayU, для внесения данных Вашей банковской карты.
+<br><br>2.2 Все данные, введенные Вами на платежной форме процессингового центра PayU, полностью защищены в соответствии с требованиями стандарта безопасности PCI DSS. Мы получаем информацию только о совершенном Вами платеже.
+<br><br>2.3 На указанный Вами при оформлении платежа адрес электронной почты, будет отправлено сообщение об авторизации платежа.
+<br><br>2.4 Сразу после совершения платежа вы будете перенаправлены обратно на наш сайт. Информация о вашем платеже может идти до нас от 5 секунд до нескольких минут. В случае, если по вашему мнению произошла задержка в обработке заказа, Вам необходимо обратиться в офис компании по телефону  7(499)-4040-896
+<br><br><span style="text-align: center !important; display: block; font-weight: 500;">3 Личные данные покупателя</span>
+<br><br>3.1 При оформлении Заявки на Сайте Клиент предоставляет следующую информацию: Фамилия, Имя, адрес электронной почты, телефон, адрес доставки.
+<br><br>3.2 Продавец использует информацию для выполнения своих обязательств перед Клиентом. Продавец обязуется не разглашать полученную от Клиента информацию. Не считается нарушением предоставление Продавцом информации агентам и третьим лицам, действующим на основании договора с Продавцом, для исполнения обязательств перед Клиентом.
+<br><br>3.3 Не считается нарушением обязательств разглашение информации в соответствии с обоснованными и применимыми требованиями закона. Продавец не несет ответственности за сведения, предоставленные Клиентом на Сайте в общедоступной форме.
+<br><br><span style="text-align: center !important; display: block; font-weight: 500;">4 Гарантии безопасности</span>
+<br><br>4.1 Платежная система Wallet One лицензируема надзорным органом в каждой стране присутствия, а значит, полностью безопасна. Wallet One использует стандарт безопасности PCI DSS, SSL-протокол, системы безопасности Verified by Visa и MasterCard SecureCode. PCI DSS — международный стандарт безопасности, разработанный Советом по стандартам безопасности индустрии платежных карт (Payment Card Industry Security Standards Council). Представляет собой 12 требований к построению и обслуживанию безопасных систем и принят крупнейшими международными платежными системами. Wallet One ежегодно проходит тестирование на соответствие стандарту PCI DSS. SSL-протокол (Secure Sockets Layer) — криптографический транспортный механизм, обеспечивающий безопасность передачи данных. Verified by Visa — технология дополнительной защиты при проведении платежей, разработанная платежной системой Visa. MasterCard SecureCode — дополнительная защита при проведении платежей с банковских карт MasterCard. А еще мы используем антифрод-систему, предназначенную для выявления мошеннических платежей. Это делает Wallet One удобной и безопасной платежной системой для всех, кроме мошенников. В случае, если у Вас есть вопросы по совершенному платежу, Вы можете обратиться в службу поддержки клиентов по электронной почте support@cloudpayments.ru.
+<br><br><span style="text-align: center !important; display: block; font-weight: 500;">5 Безопасность онлайн платежей</span>
+<br><br>5.1 Предоставляемая Вами персональная информация (имя, адрес, телефон, e-mail, номер кредитной карты) является конфиденциальной и не подлежит разглашению. Данные Вашей кредитной карты передаются только в зашифрованном виде и не сохраняются на нашем Web-сервере.
+<br><br><span style="text-align: center !important; display: block; font-weight: 500;">6 Возврат товаров</span>
+<br><br>6.1 Постановление Правительства РФ от 19 января 1998 г. В случае обнаружения недостатков товара, свойства которого не позволяют устранить их, покупатель вправе по своему выбору потребовать замены такого товара товаром надлежащего качества либо соразмерного уменьшения покупной цены. Вместо предъявления указанных требований покупатель вправе отказаться от приобретенного товара и потребовать возврата уплаченной за товар денежной суммы. При этом покупатель по требованию продавца и за свой счет должен возвратить полученный товар ненадлежащего качества.
+<br><br>6.2 В случае требования возврата денежных средств по причинам, отличным от указанных в п.4.1, продавец в праве удержать комиссию за возврат в размере до 7%, необходимую для компенсации части издержек за переводы денежных средств (при оплате товара и возврате денежных средств).
+<br><br><span style="text-align: center !important; display: block; font-weight: 500;">7 Доступные способы оплаты</span>						<div class="social" style="display: inline-block;">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/evroset.png" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/logo_alfa.gif" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/maestro_100.jpg" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/mc_logo.gif" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/mc_securecode.gif" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/qiwi.jpg" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/svyaznoy.png" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/visa_el.jpg" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/visa_logo.gif" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/visa_verified.gif" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/webmoney.jpg" alt="">
+								<img style="display: inline-block; width: 79px; vertical-align: middle;" src="http://inc.masaa.ru/pay/img/pay_u.jpg" alt="">
+							</div>
+              <br><br><span style="text-align: center !important; display: block; font-weight: 500;">8 Видео-инструкции по основным способам оплаты</span>
+              <br><br>а) <a href="https://www.youtube.com/watch?v=wMsJQ8dx1Dc" target="_blank">Пластиковая карта</a>
+              <br><br>б) <a href="https://www.youtube.com/watch?v=6wJvSqH6MuU" target="_blank">Сбербанк онлайн</a>
+              <br><br>в) <a href="https://www.youtube.com/watch?v=uGnYHsyeRlQ" target="_blank">Евросеть/Связной</a>
+              <br><br>г) <a href="https://www.youtube.com/watch?v=1womtdI3RpE" target="_blank">PayPal</a>
+              <br><br>д) <a href="https://www.youtube.com/watch?v=9SihAcAG7Xc" target="_blank">Яндекс.Деньги</a>
+              <br><br>е) <a href="https://www.youtube.com/watch?v=DXKaTlJDNt0" target="_blank">Пластиковая карта из заграницы</a>
+              <br><br><span style="text-align: center !important; display: block; font-weight: 500;">9 Актуальность информации</span>
+              <br><br>Информация на данной странице актуальна для сайта <a href="https://www.masaa.ru">masaa.ru</a> и для его поддоменов, а также для сайтов партнеров и их поддоменов: <a href="https://www.masaa.ru">masaa.ru</a> и <a href="https://www.masteryschool.ru/">masteryschool.ru</a>
+</div>
+
+ <? include '/var/www/inc.masaa.ru/inc/footer/footer.php';?>
+
+</body>
+</html>
