62ab72e910919b6d5614fad6f257fadd1a202745
Alexey Lobyak <crytekunitytek@gmail.com>
2017-07-24 16:12:23 +0300

    Rewriting history.

--- masaa_php_landing/include/confirm.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head>
-<body>
-<?= include_once('/var/www/inc.masaa.ru/inc/confirm.php'); ?>
-</body>
-</html>
