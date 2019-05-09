d204d67722e2ef0ed916bb768542cb2be9591406
Alexey Lobyak <alexey.lobyak@solbeg.com>
2019-05-09 05:05:56 +0300

    added react project

---
+++ silverReact/precache-manifest.d05fc21702e1bff6b18fd0669d0a61d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8349929040c580824a89adc795d65164",
+    "url": "/silverDemo/index.html"
+  },
+  {
+    "revision": "0942783da007117a0ee9",
+    "url": "/silverDemo/static/css/main.596e3e08.chunk.css"
+  },
+  {
+    "revision": "81a60fb96010d51b8255",
+    "url": "/silverDemo/static/js/2.a642394c.chunk.js"
+  },
+  {
+    "revision": "0942783da007117a0ee9",
+    "url": "/silverDemo/static/js/main.a1e384c5.chunk.js"
+  },
+  {
+    "revision": "6ca7a8b1c8320964e61c",
+    "url": "/silverDemo/static/js/runtime~main.4fd6c882.js"
+  }
+]);
